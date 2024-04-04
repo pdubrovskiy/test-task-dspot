51296f393c59befa427de2762aa6d2a60970fd3b
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2024-04-05 01:28:50 +0300

    feat: Scripts for running seeds implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { DatabaseModule } from './database/database.module';
 import { winstonConfig } from './config/winston.config';
 import { AppConfigModule } from './config/app-config.module';
 import { ProfilesModule } from './core/profiles/profiles.module';
+import { SeederModule } from './database/seeders/seeders.module';
 
 @Module({
-  imports: [AppConfigModule, WinstonModule.forRoot(winstonConfig), DatabaseModule, ProfilesModule],
+  imports: [
+    AppConfigModule,
+    SeederModule,
+    WinstonModule.forRoot(winstonConfig),
+    DatabaseModule,
+    ProfilesModule,
+  ],
 })
 export class AppModule {}
